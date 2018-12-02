f51dbeb04d4e56657d46ad52680320f7f119105d
Francesco Bedussi <francesco@francescobedussi.it>
2018-12-02 16:44:32 +0100

    Updates

---
+++ precache-manifest.bfb031f52f5c4af4f2f744943cfe8065.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "6e21ae19d9c111ce6fb7",
+    "url": "/store-locator-react/static/js/runtime~main.6e21ae19.js"
+  },
+  {
+    "revision": "8fa318a23b33273226aa",
+    "url": "/store-locator-react/static/js/main.8fa318a2.chunk.js"
+  },
+  {
+    "revision": "f70ca12eb16f4623e7a0",
+    "url": "/store-locator-react/static/js/1.f70ca12e.chunk.js"
+  },
+  {
+    "revision": "8fa318a23b33273226aa",
+    "url": "/store-locator-react/static/css/main.6f66559e.chunk.css"
+  },
+  {
+    "revision": "849c2dc90bc0d5612141879bb5bde2f5",
+    "url": "/store-locator-react/index.html"
+  }
+];
